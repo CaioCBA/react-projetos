f5960e0d476b6bd99dc3a2e9612493872ae277d7
Pedro Henrique PHD <danesvara2002@gmail.com>
2022-06-14 18:10:03 -0300

    Revert "Update index.js"

--- screens/Profile/styles.js
+++ screens/Profile/styles.js
@@ -2,4 +2,7 @@ import React from "react";
 
 import styled from "styled-components/native";
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: #1e1d21;
+`;
