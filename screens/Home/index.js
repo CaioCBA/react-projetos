003a2f0f93c4526adc52822e3160b044423127f0
Caio César <caiocesar132016@gmail.com>
2022-06-14 17:42:25 -0300

    Update index.js

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -10,7 +10,14 @@ import {
 
 import SearchIcon from "../../src/assets/search.svg";
 import { useNavigation } from "@react-navigation/native";
-import { FlatList, SafeAreaView, Text, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  Image,
+} from "react-native";
 
 export default () => {
   const navigation = useNavigation();
@@ -22,11 +29,23 @@ export default () => {
           Encontre aqui o que você quer comprar!
         </HeaderTitle>
       </HeaderArea>
+      <Image
+        style={styles.containerImg}
+        source={require("../../imagens-aplicativo/conjunto-de-frutas-vegetais-e-objetos-de-conjunto-de-agua-isolados-no-fundo-branco-para-vegetarianos-e-curar_65580-234.jpg")}
+      />
     </Container>
   );
 };
 
 const styles = StyleSheet.create({
+  containerImg: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    width: "100%",
+    height: "50%",
+  },
   container: {
     flex: 1,
     backgroundColor: "#FFF",
